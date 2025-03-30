b0ccc9aba9bd8e75b4da20fb231e6d9131eb93e4
monarkkurosaki <monarkyadav@gmail.com>
2025-03-30 21:47:51 +0545

    first commit

---
+++ DiceRollApp/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace DiceRollApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int playerRollNum;
+
+        Random random = new Random();
+        for (int i = 0; i == 9; i++)
+        {
+            Console.WriteLine("Press any key to roll the dice");
+
+            playerRollNum = random.Next(1, 7);
+            
+            Console.WriteLine("The number you rolled is: " + playerRollNum);
+        }
+    }
+}
